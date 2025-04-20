073ed2d6cb00b203c9f933f66b43f99f3644fa87
Taku Egami <egami@genron.co.jp>
2025-04-21 03:07:49 +0900

    lib_setting

---
+++ lib/supabase.ts
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js'
+
+export const supabase = createClient(
+  process.env.https://jwybgokjzcqpmqncibdg.supabase.co!,
+  process.env.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp3eWJnb2tqemNxcG1xbmNpYmRnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDUxNjk5MzMsImV4cCI6MjA2MDc0NTkzM30.YsiOZvkW8hpq6xbytLg6VYM_Ap7YuySkL4qvSDf-cGY!
+)
